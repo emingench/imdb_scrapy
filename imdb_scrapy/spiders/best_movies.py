cfeca21526137c77351dd4073268e727e1e77523
emingenc <emingench@gmail.com>
2021-03-03 17:47:18 +0300

    project completed

--- imdb_scrapy/spiders/best_movies.py
+++ imdb_scrapy/spiders/best_movies.py
@@ -7,11 +7,23 @@ from scrapy.spiders import CrawlSpider, Rule
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
     allowed_domains = ['imdb.com']
-    start_urls = ['https://www.imdb.com/search/title/?groups=top_250&sort=user_rating,desc']
+
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.imdb.com/search/title/?groups=top_250&sort=user_rating', headers={
+            'User-Agent': self.user_agent
+        })
 
     rules = (
-        Rule(LinkExtractor(restrict_xpaths="//h3[@class='lister-item-header']"), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//h3[@class='lister-item-header']"), callback='parse_item', follow=True, process_request='set_user_agent'),
+        Rule(LinkExtractor(restrict_xpaths="(//a[@class='lister-page-next next-page'])[1]"), process_request='set_user_agent'),
     )
+
+    def set_user_agent(self, request):
+        request.headers['User-Agent'] = self.user_agent
+        return request
+
 
     def parse_item(self, response):
         yield {
