c7e0a2e81c8488563c68d3c8bda6f5a1f66201f5
emingenc <emingench@gmail.com>
2021-03-04 12:15:16 +0300

    sqlite save

--- imdb_scrapy/pipelines.py
+++ imdb_scrapy/pipelines.py
@@ -4,8 +4,51 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import sqlite3
 
 class ImdbScrapyPipeline(object):
     def process_item(self, item, spider):
         return item
+
+class SQLlitePipeline(object):
+
+    def open_spider(self, spider):
+        self.connection = sqlite3.connect("imdb.db")
+        self.c = self.connection.cursor()
+        try:
+            self.c.execute('''
+                CREATE TABLE  IF NOT EXISTS best_movies(
+                    title TEXT,
+                    year TEXT,
+                    duration TEXT,
+                    genre TEXT,
+                    rating TEXT,
+                    movie_url TEXT
+                )
+            
+            ''')
+            self.connection.commit()
+        except sqlite3.OperationalError:
+            pass
+
+    def close_spider(self, spider):
+        self.connection.close()
+
+
+    def process_item(self, item, spider):
+        self.c.execute('''
+            INSERT INTO best_movies (title,year,duration,genre,rating,movie_url) VALUES(?,?,?,?,?,?)
+
+        ''', (
+            item.get('title'),
+            item.get('year'),
+            item.get('duration'),
+            item.get('genre'),
+            item.get('rating'),
+            item.get('movie_url')
+        ))
+        self.connection.commit()
+        return item
+
+
+
